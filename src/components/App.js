59144acb545de19397c0ad7750ba8a261f353a9f
Pascal <pascalrintareseketeti@gmail.com>
2025-04-11 10:17:39 +0300

    adding hooks and imports

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,28 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+
+
+function About() {
+  return (
+    <div id="about">About
+     <h2> 
+      about
+     </h2>
+    </div>
+  )
+}
+
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
+      
     </div>
   );
 }
